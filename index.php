0f8aa8945d923308d32e96a59d3a195ed7f70b32
Babai Bose <niyooshawebsites@gmail.com>
2022-12-22 12:51:43 +0530

    some changes made

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Home</title>
+</head>
+<body>
+    <div class="wrapper">
+        <header class="header">
+            <nav class="nav-bar">
+                <div class="brand">
+                    <img src="" alt="" class="logo">
+                </div>
+                <ul class="nav">
+                    <li class="nav-item"><a href="" class="nav-link">Home</a></li>
+                    <li class="nav-item"><a href="" class="nav-link">Services</a></li>
+                    <li class="nav-item"><a href="" class="nav-link">About</a></li>
+                    <li class="nav-item"><a href="" class="nav-link">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+        <section class="hero">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi minima laboriosam dolores soluta beatae omnis non eveniet? Et, natus fuga, autem magnam similique deleniti facilis saepe repellendus, neque cumque tenetur.</p>
+        </section>
+        <footer class="footer"></footer>
+    </div>
+</body>
+</html>
